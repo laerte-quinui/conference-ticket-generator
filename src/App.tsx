ab84a9e460d41432e4827a2699731a73481a2c19
Laerte Quinui <laertequinuio@gmail.com>
2025-07-10 10:25:39 -0300

    feat: Build the Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Logo from './assets/images/logo-full.svg'
 import Background from './components/Background'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
+      <div className="container px-4">
+        <img src={Logo} className="mx-auto mt-8 mb-16" />
+        <Header />
+      </div>
       <Background />
     </div>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="mx-auto flex w-full flex-col justify-center gap-4 text-center sm:max-w-3xl md:max-w-9/12 lg:max-w-1/2">
+      <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
+      <p className="text-[var(--neutral-300)]">
+        Secure your spot at next year's biggest coding conference.
+      </p>
+    </div>
+  )
+}
+
+export default Header
