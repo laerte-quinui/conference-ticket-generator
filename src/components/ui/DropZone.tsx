cd6bf442e7c626697e8612fdc76fe2c706653faf
Laerte Quinui <laertequinuio@gmail.com>
2025-07-11 14:08:33 -0300

    feat: Adding the add image functionality

--- src/components/ui/DropZone.tsx
+++ src/components/ui/DropZone.tsx
@@ -1,28 +1,49 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import UploadIcon from '../../assets/images/icon-upload.svg'
 import '../../styles/drop-zone.css'
 
 const DropZone = () => {
-  interface FileWithPath extends File {
-    path?: string
-  }
+  const [avatarUrl, setAvatarUrl] = useState<string | null>()
 
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
-    acceptedFiles.forEach((file: FileWithPath) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file: File) => {
       const reader = new FileReader()
       reader.onabort = () => console.log('file reading was aborted')
       reader.onerror = () => console.log('file reading has failed')
       reader.onload = () => {
-        console.log('file dropped:', file)
         const binaryStr = reader.result
-        console.log(binaryStr)
+        if (typeof binaryStr === 'string' || binaryStr === null)
+          setAvatarUrl(binaryStr)
       }
-      reader.readAsArrayBuffer(file)
+      reader.readAsDataURL(file)
     })
   }, [])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  if (avatarUrl) {
+    return (
+      <div className="drop-zone">
+        <div className="drop-zone__icon with-avatar">
+          <img src={avatarUrl} />
+        </div>
+        <div className="flex items-center gap-2">
+          <button
+            type="button"
+            className="btn-gray underline"
+            onClick={() => setAvatarUrl(null)}
+          >
+            Remove image
+          </button>
+          <label className="btn-gray !font-normal !text-[var(--neutral-300)]">
+            Change image
+            <input className="hidden" type="file" {...getInputProps()} />
+          </label>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div
