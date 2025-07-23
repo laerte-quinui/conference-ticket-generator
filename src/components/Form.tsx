c92801260add2663b19809249480321e4c829a30
Laerte Quinui <laertequinuio@gmail.com>
2025-07-23 16:20:59 -0300

    feat: Implement erros validation in the form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,34 @@
 import { Controller, useForm } from 'react-hook-form'
 import { useSubmit } from 'react-router'
+import ErrorIcon from '../assets/images/icon-error.svg'
 import InfoIcon from '../assets/images/icon-info.svg'
 import '../styles/form.css'
 import DropZone from './ui/DropZone'
 
 type FormType = {
-  avatar: File
+  avatar: File[]
   userName: string
   email: string
   github: string
 }
 
 const Form = () => {
-  const { handleSubmit, register, control } = useForm<FormType>()
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors }
+  } = useForm<FormType>()
   const submit = useSubmit()
 
   const onSubmit = (data: FormType) => {
-    console.log(data.avatar)
     const id = String(Math.floor(Math.random() * 100000)).padStart(5, '0')
     const formData = new FormData()
+
     Object.entries({ ...data, id }).forEach(([key, value]) => {
-      formData.append(key, value)
+      if (key === 'avatar' && Array.isArray(value))
+        value.forEach(file => formData.append('avatar', file))
+      else formData.append(key, String(value))
     })
 
     submit(formData, { method: 'post', action: '/ticket/' + id })
@@ -39,15 +47,34 @@ const Form = () => {
         <Controller
           name="avatar"
           control={control}
+          rules={{
+            required: 'Please, upload your avatar.',
+            validate: {
+              acceptedFormats: file =>
+                file[0]?.type === 'image/jpeg' ||
+                file[0]?.type === 'image/png' ||
+                'Only JPG or PNG files are allowed.',
+              maxSize: file =>
+                file[0]?.size <= 500000 ||
+                'File too large. Please upload a photo that is less than 500KB.'
+            }
+          }}
           render={({ field: { onChange } }) => (
             <DropZone onFileChange={onChange} />
           )}
         />
 
-        <span className="mt-2 flex items-center gap-2 text-xs text-[var(--neutral-500)]">
-          <img src={InfoIcon} alt="Info Icon" className="h-4 w-4" />
-          Upload your photo (JPG or PNG, max size: 500KB)
-        </span>
+        {errors.avatar ? (
+          <span className="tip-message error">
+            <img src={ErrorIcon} alt="Error Icon" className="h-4 w-4" />
+            {errors.avatar.message}
+          </span>
+        ) : (
+          <span className="tip-message">
+            <img src={InfoIcon} alt="Info Icon" className="h-4 w-4" />
+            Upload your photo (JPG or PNG, max size: 500KB)
+          </span>
+        )}
       </div>
 
       {/* Full name */}
@@ -59,9 +86,15 @@ const Form = () => {
           id="userName"
           type="text"
           placeholder="Jhon Doe"
-          {...register('userName', { required: true })}
-          className="input"
+          {...register('userName', { required: 'Please, enter your name.' })}
+          className={`input ${errors.userName ? 'error' : ''}`}
         />
+        {errors.userName && (
+          <span className="tip-message error">
+            <img src={ErrorIcon} alt="Error Icon" className="h-4 w-4" />
+            {errors.userName.message}
+          </span>
+        )}
       </div>
 
       {/* Email */}
@@ -73,9 +106,22 @@ const Form = () => {
           id="email"
           type="email"
           placeholder="example@email.com"
-          {...register('email', { required: true })}
-          className="input"
+          {...register('email', {
+            required: 'Please, enter your email.',
+            validate: {
+              isEmail: value =>
+                /^\S+@\S+\.\S+$/.test(value) ||
+                'Please, enter a valid email address.'
+            }
+          })}
+          className={`input ${errors.email ? 'error' : ''}`}
         />
+        {errors.email && (
+          <span className="tip-message error">
+            <img src={ErrorIcon} alt="Error Icon" className="h-4 w-4" />
+            {errors.email.message}
+          </span>
+        )}
       </div>
 
       {/* GitHub */}
@@ -87,9 +133,17 @@ const Form = () => {
           id="github"
           type="text"
           placeholder="@yourusername"
-          {...register('github', { required: true })}
-          className="input"
+          {...register('github', {
+            required: 'Please, enter your GitHub username.'
+          })}
+          className={`input ${errors.github ? 'error' : ''}`}
         />
+        {errors.github && (
+          <span className="tip-message error">
+            <img src={ErrorIcon} alt="Error Icon" className="h-4 w-4" />
+            {errors.github.message}
+          </span>
+        )}
       </div>
 
       <button className="btn" type="submit">
